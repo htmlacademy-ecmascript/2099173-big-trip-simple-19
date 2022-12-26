49e608f7b8bc02d22279b6b8f671a5deacda7338
Andrei Gladyshev <mytime12-61pm@yandex.ru>
2022-12-26 13:40:40 +0400

    Подготавливает файлы к следующему ДЗ

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -5,6 +5,8 @@ const POINT_COUNT = 4;
 export default class PointsModel {
   #points = Array.from({length: POINT_COUNT}, getRandomPoint);
 
+  // Array.from({length: POINT_COUNT}, getRandomPoint);
+
   get points() {
     return this.#points;
   }
