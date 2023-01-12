de43a5ccf40119ac4b6595082b33cd5498ffea54
Andrei Gladyshev <mytime12-61pm@yandex.ru>
2023-01-12 20:11:44 +0400

    подготавливает проект к дз из раздела 5.7. Большие перемены (часть 1)

---
+++ src/presenter/points-presenter.js
@@ -0,0 +1,109 @@
+import {render, replace, remove} from '../framework/render.js';
+import PointView from '../view/point-view.js';
+import EditPointFormView from '../view/edit-point-view.js';
+
+const Mode = {
+  DEFAULT: 'DEFAULT',
+  EDITING: 'EDITING'
+};
+
+export default class PointPresenter {
+
+  #pointItemComponent = null;
+  #pointComponent = null;
+  #editPointComponent = null;
+  #point = null;
+  #mode = Mode.DEFAULT;
+  #handleModeChange = null;
+
+  constructor({pointItemComponent, onModeChange}) {
+    this.#pointItemComponent = pointItemComponent;
+    this.#handleModeChange = onModeChange;
+  }
+
+  init (point) {
+    this.#point = point;
+
+    const prevPointComponent = this.#pointComponent;
+    const prevEditPointComponent = this.#editPointComponent;
+
+    this.#pointComponent = new PointView({
+      point: this.#point,
+      onEditClick: this.#handleEditClick,
+    });
+
+    this.#editPointComponent = new EditPointFormView ({
+      point: this.#point,
+      onFormSubmit: this.#handleFormSubmit,
+      onFormClose: this.#handleFormClose,
+    });
+
+    if (prevPointComponent === null || prevEditPointComponent === null) {
+      render(this.#pointComponent, this.#pointItemComponent);
+      return;
+    }
+
+    if (this.#mode === Mode.DEFAULT) {
+      replace (this.#pointComponent, prevPointComponent);
+    }
+
+    if (this.#mode === Mode.EDITING) {
+      replace(this.#editPointComponent, prevEditPointComponent);
+    }
+
+    if (this.#pointItemComponent.contains(prevPointComponent.element)) {
+      replace(this.#pointComponent, prevPointComponent);
+    }
+
+    if (this.#pointItemComponent.contains(prevEditPointComponent.element)) {
+      replace(this.#editPointComponent, prevEditPointComponent);
+    }
+
+    remove(prevPointComponent);
+    remove(prevEditPointComponent);
+  }
+
+  destroy() {
+    remove(this.#pointComponent);
+    remove(this.#editPointComponent);
+  }
+
+  resetView() {
+    if (this.#mode !== Mode.DEFAULT) {
+      this.#replaceFormToPoint();
+    }
+  }
+
+  #replacePointToForm() {
+    replace(this.#editPointComponent, this.#pointComponent);
+    document.addEventListener('keydown', this.#escKeyDownHandler);
+    this.#handleModeChange();
+    this.#mode = Mode.EDITING;
+  }
+
+  #replaceFormToPoint() {
+    replace(this.#pointComponent, this.#editPointComponent);
+    document.removeEventListener('keydown', this.#escKeyDownHandler);
+    this.#mode = Mode.DEFAULT;
+  }
+
+  #escKeyDownHandler = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      this.#replaceFormToPoint();
+    }
+  };
+
+
+  #handleEditClick = () => {
+    this.#replacePointToForm();
+  };
+
+  #handleFormSubmit = () => {
+    this.#replaceFormToPoint();
+  };
+
+  #handleFormClose = () => {
+    this.#replaceFormToPoint();
+  };
+}
