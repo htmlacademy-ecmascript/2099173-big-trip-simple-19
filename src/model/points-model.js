3394d8d6933bf8ca0a75b1c8ec77f314ab22529a
Andrei Gladyshev <mytime12-61pm@yandex.ru>
2023-02-09 10:54:17 +0400

    доделывает домашнее задание в разделе 8.7. Пришёл, увидел, загрузил (часть 1)

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -71,7 +71,7 @@ export default class PointsModel extends Observable{
         ...this.#points.slice(index + 1),
       ];
 
-      this._notify(updateType, update);
+      this._notify(updateType, updatedPoint);
     } catch(err) {
       throw new Error('Can\'t update point');
     }
