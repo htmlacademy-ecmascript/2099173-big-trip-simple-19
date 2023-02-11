612e9722cef16a38486641fffe8787263f0fff14
Andrei Gladyshev <mytime12-61pm@yandex.ru>
2023-02-11 13:43:39 +0400

    Подготавливает проект к заключительной проверке

--- src/presenter/points-list-presenter.js
+++ src/presenter/points-list-presenter.js
@@ -46,7 +46,7 @@ export default class PointsListPresenter {
       offers: this.#pointsModel.offers,
       pointListContainer: this.#pointListComponent.element,
       onDataChange: this.#handleViewAction,
-      onDestroy: onNewPointDestroy
+      onDestroy: onNewPointDestroy,
     });
 
     this.#pointsModel.addObserver(this.#handleModelEvent);
@@ -91,10 +91,6 @@ export default class PointsListPresenter {
     this.#newPointPresenter.destroy();
     this.#pointPresenter.forEach((presenter) => presenter.resetView());
   };
-
-  // #handlePointChange = (updatedPoint) => {
-  //   this.#pointPresenter.get(updatedPoint.id).init(updatedPoint);
-  // };
 
   #handleViewAction = async (actionType, updateType, update) => {
     this.#uiBlocker.block();
